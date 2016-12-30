b420f556df49cbe0628f1825f1a0cfc32c89b9d0
Attonasi <attonasi@hotmail.com>
2016-12-30 13:53:53 -0800

    trying to make stuff work

--- app4.js
+++ app4.js
@@ -1,9 +1,13 @@
 (function () {
-  var PRIV_KEY = "2dee256aac3f0163db8093457368ac4d13f8dc72";
-  var PUBLIC_KEY = "f9420321c18cd89cc9801877c864b290";
-  var characterNames = [];
-  var characterIDs = [];
+  var PRIV_KEY = '2dee256aac3f0163db8093457368ac4d13f8dc72';
+  var PUBLIC_KEY = 'f9420321c18cd89cc9801877c864b290';
+  var Marvel = function() {
+    this.characterIDs = [],
+    this.characterNames = []
+  }();
 
+
+  console.log('hey');
   function getMarvelResponse() {
 
     // you need a new ts every request
@@ -11,7 +15,7 @@
     var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
 
     // the api deals a lot in ids rather than just the strings you want to use
-    var characterId = '1009718'; // wolverine
+    // var characterId = '1009718'; // wolverine
 
 
     var url = 'https://gateway.marvel.com/v1/public/characters';
@@ -27,29 +31,27 @@
         hash: hash,
         offset: num,
         limit: 100,
-        })
+      })
 
-        .done(function(data) {
-          // sort of a long dump you will need to sort through
-          // console.log(data);
-          // console.log(data.data.results[26].name);
-          for(var j = 0; j < 100; j++){
-            if(data.data.results[j].id === undefined){
-              console.log(data.data);
-            }
-            if(data.data.results[j].id){
-              characterIDs.push(data.data.results[j].id);
-              characterNames.push(data.data.results[j].name);
-            }
+      .done(function(data) {
+        // sort of a long dump you will need to sort through
+        console.log(data);
+        // console.log(data.data.results.name);
+        for(var j = 0; j < 100; j++){
+          if(data.data.results[j].id){
+            Marvel.characterIDs.push(data.data.results[j].id);
+            Marvel.characterNames.push(data.data.results[j].name);
           }
-          console.log(characterIDs);
-        })
-        .fail(function(err){
-          // the error codes are listed on the dev site
-          console.log(err);
-        });
+        }
+        console.log(Marvel.characterIDs);
+      })
+      .fail(function(err){
+        // the error codes are listed on the dev site
+        console.log(err);
+      });
     }
-  };
+  }
 
+  getMarvelResponse();
 
-});
+})();
