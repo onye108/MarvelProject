ab7197a06b2852d6d77f9f5d5b8ecbabc16027d0
Kevia <cloud@kevia.me>
2016-12-30 14:34:40 -0800

    clean

--- app.js
+++ app.js
@@ -1,39 +1,26 @@
 (function () {
+'use strict';
 
-  // you will also have to setup the referring domains on your marvel developer portal
-  var PRIV_KEY = "2dee256aac3f0163db8093457368ac4d13f8dc72";
-  var PUBLIC_KEY = "f9420321c18cd89cc9801877c864b290";
+var MarvelSingleton = {
+  privateKey: "2dee256aac3f0163db8093457368ac4d13f8dc72",
+  publicKey: "f9420321c18cd89cc9801877c864b290",
+  characterID: "1009718",
+  url: 'https://gateway.marvel.com/v1/public/characters/',
 
-  function getMarvelResponse() {
+  generateUrl: function() {
+    return this.url + this.characterID;
+  },
 
-    // you need a new ts every request
-    var ts = new Date().getTime();
-    var hash = CryptoJS.MD5(ts + PRIV_KEY + PUBLIC_KEY).toString();
+  generateTimeStamp: function() {
+    return new Date().getTime();
+  },
 
-    // the api deals a lot in ids rather than just the strings you want to use
-    var characterId = '1009718'; // wolverine
+  generateHash: function() {
+    return CryptoJS.MD5(this.generateTimeStamp() + this.privateKey + this.publicKey).toString();
+  }
 
+};
 
-    var url = 'https://gateway.marvel.com/v1/public/characters/' + characterId;
-
-    $.getJSON(url, {
-      ts: ts,
-      apikey: PUBLIC_KEY,
-
-      hash: hash
-
-      })
-      .done(function(data) {
-        // sort of a long dump you will need to sort through
-        console.log(data.data);
-      })
-      .fail(function(err) {
-        // the error codes are listed on the dev site
-        console.log(err);
-      });
-  };
-
-  getMarvelResponse();
-
+  getMarvelResponse(MarvelSingleton);
 
 })();
