4ce0755cf4ea6a69d8d9cf6c2023cf5606905478
Attonasi <attonasi@hotmail.com>
2016-12-30 13:11:04 -0800

    merge conflicts eliminated

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-
+(function () {
 
   // you will also have to setup the referring domains on your marvel developer portal
   var PRIV_KEY = "2dee256aac3f0163db8093457368ac4d13f8dc72";
   var PUBLIC_KEY = "f9420321c18cd89cc9801877c864b290";
-  var characterNames = [];
-  var characterIDs = [];
 
   function getMarvelResponse() {
 
@@ -16,41 +14,24 @@
     var characterId = '1009718'; // wolverine
 
 
-    var url = 'https://gateway.marvel.com/v1/public/characters';
+    var url = 'https://gateway.marvel.com/v1/public/characters/' + characterId;
 
-    console.log(url);
-
-    for(var i = 0; i < 20; i++){
-      var num = i * 100;
-
-      $.getJSON(url, {
-        ts: ts,
-        apikey: PUBLIC_KEY,
-        hash: hash,
-        offset: num,
-        limit: 100,
-        })
-
-        .done(function(data) {
-          // sort of a long dump you will need to sort through
-          // console.log(data);
-          // console.log(data.data.results[26].name);
-          for(var j = 0; j < 100; j++){
-            if(data.data.results[j].id === undefined){
-              console.log(data.data);
-            }
-            if(data.data.results[j].id){
-              characterIDs.push(data.data.results[j].id);
-              characterNames.push(data.data.results[j].name);
-            }
-          }
-          console.log(characterIDs);
-        })
-        .fail(function(err){
-          // the error codes are listed on the dev site
-          console.log(err);
-        });
-    }
+    $.getJSON(url, {
+      ts: ts,
+      apikey: PUBLIC_KEY,
+      hash: hash
+      })
+      .done(function(data) {
+        // sort of a long dump you will need to sort through
+        console.log(data.data);
+      })
+      .fail(function(err) {
+        // the error codes are listed on the dev site
+        console.log(err);
+      });
   };
 
   getMarvelResponse();
+
+
+})();
